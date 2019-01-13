4b9bdb5d56a475663bd271ed0bd164e2d725d27d
KellenCheng <kken339039@yahoo.com.tw>
2019-01-13 16:26:36 +0800

    Modify image and product model and fix not admin user can create product

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ApplicationRecord
-  has_many :product_images, dependent: :destroy
+  has_many :product_images, dependent: :delete_all
   has_many :products, through: :product_images, source: :product
 
   mount_uploader :image, ImageUploader
