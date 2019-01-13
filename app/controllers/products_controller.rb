94056dc9ee391c6cd97420ae5cca8bead3cac0f6
KellenCheng <kken339039@yahoo.com.tw>
2019-01-13 14:34:30 +0800

    Add feature product update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authorize_admin, except: [:index]
+  before_action :set_product, only: [:edit, :update]
 
   def index
     @products = Product.all.order(id: :DESC)
@@ -7,9 +8,6 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
-  end
-
-  def edit
   end
 
   def create
@@ -25,11 +23,23 @@ class ProductsController < ApplicationController
   end
 
   def update
+    mes = "Update Product"
+    if @product.update!(product_params)
+      flash[:notice] = mes + " Success"
+      redirect_to root_path
+    else
+      flash[:alert] = mes + " Fail"
+      redirect_back(fallback_location: edit_product_path)
+    end
   end
 
   private
 
   def product_params
     params.require(:product).permit(:name, :price, :image)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,11 @@
 class Image < ApplicationRecord
   has_many :product_images, dependent: :destroy
   has_many :products, through: :product_images, source: :product
+
+  mount_uploader :file, FileUploader
+
+  private
+  def product_params
+    params.require(:images).permit(:file)
+  end
 end
