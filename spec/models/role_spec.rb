b8dcaf74098f20ec8712c230f5b7f0f89cd85af2
KellenCheng <kken339039@yahoo.com.tw>
2019-01-14 16:40:29 +0800

    Add role rspec

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe Role, type: :model do
     it { should have_many(:user_roles) }
     it { should have_many(:users) }
   end
+
+  describe 'Model validation' do
+    it 'unique role name' do
+      role = Faker::Name.name
+      exist_role = create(:role, name: role)
+      build(:role, name: role).should be_invalid
+    end
+  end
 end
