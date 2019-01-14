591e4e0374f018b1d865f7c729649c42ac9be3c6
KellenCheng <kken339039@yahoo.com.tw>
2019-01-14 22:11:08 +0800

    Add product rpsec with normal login

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
   before do
-    @product = FactoryBot.create(:product)
     @image = Rack::Test::UploadedFile.new(Rails.root.join("spec/files/images/test.png"))
+    @product = FactoryBot.create(:product)
+    @new_product = { name: "product_test", price: 2000, image: @image }
   end
 
   describe "Without login user" do
@@ -19,13 +20,7 @@ RSpec.describe ProductsController, type: :controller do
     end
 
     it "#create new product" do
-      post :create, params: { 
-                      product: { 
-                        name: "product_test", 
-                        price: 2000, 
-                        image: @image
-                        }
-                      }
+      post :create, params: { product: @new_product }
 
       expect(response).to have_http_status(302)
       expect(response).to redirect_to(root_path)
@@ -40,12 +35,47 @@ RSpec.describe ProductsController, type: :controller do
     it "#update product" do
       patch :update, params: { 
                       id: @product.id,
-                      product: { 
-                        name: "test",
-                        price: 5000, 
-                        image: @image 
-                        } 
-                      }
+                      product: @new_product.merge(price: 5000)
+                    }
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(root_path)
+    end
+  end
+
+  describe "Login as normal user" do
+    before do
+      normal_login
+    end
+
+    it "#list products" do
+      get :index
+      expect(response).to render_template(:index)
+      expect(response).to have_http_status(200)
+    end
+
+    it "#get new product" do
+      get :new
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(root_url)
+    end
+
+    it "#create new product" do
+      post :create, params: { product: @new_product }
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(root_path)
+    end
+
+    it "#edit product" do
+      get :edit, params: { id: @product.id }
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(root_url)
+    end
+
+    it "#update product" do
+      patch :update, params: { 
+                      id: @product.id,
+                      product: @new_product.merge(price: 5000)
+                    }
       expect(response).to have_http_status(302)
       expect(response).to redirect_to(root_path)
     end
